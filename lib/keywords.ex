1f19bd0b8f243130322b15dda3cb649cde79c414
PatriciaChebet <patriciacchebet@gmail.com>
2022-05-15 22:15:18 +0300

    Compute unit price

--- lib/keywords.ex
+++ lib/keywords.ex
@@ -10,14 +10,22 @@ defmodule CodeFlow.Keywords do
   @moduledoc """
   Fix or complete the code to make the tests pass.
   """
-  # alias CodeFlow.Schemas.Item
+  alias CodeFlow.Schemas.Item
+  alias CodeFlow.Fake.Item
 
   def rounded(value, opts \\ []) do
     round_decimal = Keyword.get(opts, :decimals) || 4
     Float.round(value, round_decimal)
   end
 
-  def unit_price(_item, _opts \\ []) do
-
+  def unit_price(item, opts \\ []) do
+    unit_price = item.price / item.quantity
+    mode = Keyword.get(opts, :mode, :float)
+    case mode do
+      :float ->
+        unit_price
+      :money ->
+        :erlang.float_to_binary(unit_price, decimals: 2)
+    end
   end
 end
